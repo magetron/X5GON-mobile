0a3ad63d13a75035281f89a56478d43b33fac2f0
Patrick Wu <magetron1@gmail.com>
2020-02-03 23:46:40 +0000

    add notes cell

---
+++ x5gon-mobile/Views/PlayerViewCells/notesCell.swift
@@ -0,0 +1,14 @@
+//
+//  notesCell.swift
+//  x5gon-mobile
+//
+//  Created by Patrick Wu on 03/02/2020.
+//  Copyright © 2020 x5gon. All rights reserved.
+//
+
+import Foundation
+
+class notesCell: UITableViewCell {
+
+
+}
