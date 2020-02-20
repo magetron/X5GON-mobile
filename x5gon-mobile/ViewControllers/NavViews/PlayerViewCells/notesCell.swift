9a93594f93ddadf6aef43e30461aa235a5c89ec7
Felix <yinruihu@gmail.com>
2020-02-20 22:17:52 +0000

    add auto resize vai textviewdidchange

--- x5gon-mobile/ViewControllers/NavViews/PlayerViewCells/notesCell.swift
+++ x5gon-mobile/ViewControllers/NavViews/PlayerViewCells/notesCell.swift
@@ -13,29 +13,30 @@ class notesCell: UITableViewCell, UITextViewDelegate {
     @IBOutlet weak var textView: UITextView!
     
     @IBOutlet weak var textViewHC: NSLayoutConstraint!
-    var textHolder = ""
     func customisation () {
-        textView.text = "Write Your Note Here \n"
+        textView.text = "Write Your Note Here"
         textView.textColor = UIColor.lightGray
         textView.returnKeyType = .done
         textView.delegate = self
-        textViewHC.constant = self.textView.contentSize.height
     }
     func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.text == "Write Your Note Here \n"{
+        if textView.text == "Write Your Note Here"{
             textView.text = ""
             textView.textColor = UIColor.black
         }
     }
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        print("called textview")
         if textView.text == "\n" {
             textView.resignFirstResponder()
+            return false
         }
         return true
     }
     func textViewDidEndEditing(_ textView: UITextView) {
+        print("did end editing ")
         if textView.text == ""{
-            textView.text = "Write Your Note Here \n"
+            textView.text = "Write Your Note Here"
             textView.textColor = UIColor.lightGray
         }
         textViewHC.constant = self.textView.contentSize.height
