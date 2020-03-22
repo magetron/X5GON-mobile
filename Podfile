236ccc78975de09d629c9b677a8f19453868a4bc
Patrick Wu <magetron1@gmail.com>
2020-03-22 12:25:00 +0000

    add test support on pod file

--- Podfile
+++ Podfile
@@ -1,4 +1,8 @@
 platform :ios, '13.3'
 target 'x5gon-mobile' do
 	pod 'SwiftSoup'
+
+	target 'x5gon-mobileTests' do
+		inherit! :search_paths
+	end
 end
--- x5gon-mobileTests/x5gon_mobileTests.swift
+++ x5gon-mobileTests/x5gon_mobileTests.swift
@@ -24,6 +24,7 @@ class x5gon_mobileTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(1 == 1)
     }
 
     func testPerformanceExample() {
