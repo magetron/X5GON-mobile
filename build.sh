eba8ca14249c7b2a91ba90abfca069ca1c7d0adb
Patrick Wu <magetron1@gmail.com>
2020-03-25 07:24:33 +0000

    update archive build

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 /bin/rm -rf static
 
 mkdir static
+cd static
+mkdir archive
+cd ..
 
 cp *.html static
 cp *.md static
@@ -41,3 +44,46 @@ perl -pe 's|<zero-md .* src="research.md"></zero-md>|`cat research-md.html`|ge' 
 /bin/rm -rf $research_md_html
 
 cd ..
+
+cp archive/*.html static/archive
+cp archive/*.md static/archive
+cp -r archive/css static/archive
+cp -r archive/docs static/archive
+cp -r archive/images static/archive
+cp -r archive/js static/archive/
+cp -r archive/webfonts static/archive
+cp -r archive/external static/archive
+
+cd static/archive
+
+perl -pe 's|<div id="nav-placeholder"></div>|`cat nav.html`|ge' -i *.html
+
+perl -pe 's|<div id="footer-placeholder"></div>|`cat footer.html`|ge' -i *.html
+
+research_md_html="research-md.html"
+
+echo "<zero-md>" > $research_md_html
+echo "<template>" >> $research_md_html
+echo "<style>" >> $research_md_html
+cat css/markdown.css >> $research_md_html
+echo "</style>" >> $research_md_html
+echo "<xmp>" >> $research_md_html
+cat research.md >> $research_md_html
+echo "</xmp>" >> $research_md_html
+echo "</template>" >> $research_md_html
+echo "</zero-md>" >> $research_md_html
+
+perl -pe 's|<zero-md .* src="research.md"></zero-md>|`cat research-md.html`|ge' -i research.html
+
+/bin/rm -rf external
+/bin/rm -rf research.md
+/bin/rm -rf nav.html
+/bin/rm -rf footer.html
+/bin/rm -rf $research_md_html
+
+cd ../..
+
+
+
+
+
