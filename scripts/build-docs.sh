dff087769ab246058bbe8efa241fe023d1b2d04d
Patrick Wu <magetron1@gmail.com>
2020-04-02 08:30:10 +0100

    update docs build script

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,5 +4,14 @@ pod install
 jazzy --clean \
 	--author 'Patrick and Felix'\
 	--module-version 0.3 \
-	--min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./docs --documentation='./*.md'
-cp -r images docs/images
+	--min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ../x5gon-mobile-public-web/ --documentation='./*.md'
+cp -r images ../x5gon-mobile-public-web/images
+
+cd ../x5gon-mobile-public-web/
+git add .
+git commit -m "update docs"
+git push -f -u origin gh-pages
+
+cd ../x5gon-mobile
+
+
