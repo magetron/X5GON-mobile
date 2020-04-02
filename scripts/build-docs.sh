4ad947b40a79373dd76dadede6e12b857c7fc58a
Patrick Wu <magetron1@gmail.com>
2020-04-02 11:04:10 +0100

    attempt to fix jazzy

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,10 +1,9 @@
 #/bin/sh
 
 pod install
-jazzy --clean \
-	--author 'Patrick and Felix'\
-	--module-version 0.3 \
-	--min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./docs --documentation='./*.md'
+
+jazzy --clean --author 'Patrick and Felix' --module-version 0.3 --min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./docs --documentation='./*.md'
+
 cp -r images ./docs/images
 
 
