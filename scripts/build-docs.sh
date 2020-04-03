2c144930b22d536016b80b34675b1bf00eb307cd
Patrick Wu <magetron1@gmail.com>
2020-04-03 17:13:30 +0100

    build user manual

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -5,3 +5,4 @@ pod install
 jazzy --clean --author 'Patrick and Felix' --module-version 0.3 --build-tool-arguments -workspace,x5gon-mobile.xcworkspace,-scheme,x5gon-mobile,-sdk,iphonesimulator --min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./docs --documentation='./*.md'
 
 cp -r images ./docs/images
+cp -r user-manual/ ./docs/user-manual
